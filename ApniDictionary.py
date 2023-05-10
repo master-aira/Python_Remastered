1f170f45ee0df720d70ac741b014ea7bec986453
Paramveer Singh <109423225+master-aira@users.noreply.github.com>
2023-05-10 13:23:14 +0530

    Add files via upload

---
+++ ApniDictionary.py
@@ -0,0 +1,7 @@
+#Dictionary that takes the input from the user and returns its Value
+dic = {"YouTube":"Subscribe",
+       "Facebook":"FriendRequest",
+       "Twitter":"Follow",
+       "Whatsapp":"Number"}
+inp = input("Please Enter Your SociaL Platform")
+print(dic[inp])
